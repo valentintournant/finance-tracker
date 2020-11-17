1da417143ecbdfe4be66208e147543c11afb518d
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-17 15:03:27 +0100

    Setup UserStock resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-User.create(username: "Valentin", password: "password1234")
-User.create(username: "Sarah", password: "password12345")
