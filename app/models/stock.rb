d957b9b0a5847c3d5978b4bfb4aa7137464a58b8
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-17 14:40:50 +0100

    Users and stocks: many-to-many association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,13 @@
 class Stock < ApplicationRecord
+has_many :user_stocks
+has_many :users, through: :user_stocks
 
-  def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-                                  endpoint: 'https://sandbox.iexapis.com/v1')
-    #client.price(ticker_symbol)
+validates :name, :ticker_symbol, presence: true
+
+def self.new_lookup(ticker_symbol)
+  client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                endpoint: 'https://sandbox.iexapis.com/v1')
+  #client.price(ticker_symbol)
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
