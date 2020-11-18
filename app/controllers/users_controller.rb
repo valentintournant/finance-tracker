d326b287451f73977a31afc353cbdd35ceb50401
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-18 16:08:20 +0100

    Search users/friends: implement search method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
-  def my_portfolio
-    @tracked_stocks = current_user.stocks
-  end
+    def my_portfolio
+      @tracked_stocks = current_user.stocks
+    end
 
-  def my_friends
-    @friends = current_user.friends
+    def my_friends
+      @friends = current_user.friends
+    end
+
+    def search
+      render json: params[:friend]
+    end
+
+    def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result'}
+      end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result'}
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email"
+        format.js { render partial: 'users/friend_result'}
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+  #search fonctionalies stocks
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
     return false unless stock
     stocks.where(id: stock.id).exists?
   end
-
 
   def under_stock_limit?
     stocks.count < 10
@@ -30,4 +29,34 @@ class User < ApplicationRecord
     "Anonymous"
   end
 
+  #search fonctionalies friends
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
 end
+
+
+
+
+
+
