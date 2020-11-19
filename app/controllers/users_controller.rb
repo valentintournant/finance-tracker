c1a416ea2ffd44ece993100807de942599002cd9
Valentin Tournant <valentin.tournant@gmail.com>
2020-11-19 09:48:29 +0100

    Display user/friend profile and tracking options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,41 @@
 class UsersController < ApplicationController
-    def my_portfolio
-      @tracked_stocks = current_user.stocks
-    end
 
-    def my_friends
-      @friends = current_user.friends
-    end
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
 
-    def search
-      render json: params[:friend]
-    end
+  def my_portfolio
+    @user = current_user
+    @tracked_stocks = current_user.stocks
+  end
 
-    def search
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    render json: params[:friend]
+  end
+
+  def search
     if params[:friend].present?
       @friends = User.search(params[:friend])
       @friends = current_user.except_current_user(@friends)
-      if @friends
+        if @friends
         respond_to do |format|
-          format.js { render partial: 'users/friend_result'}
-      end
+        format.js { render partial: 'users/friend_result'}
+        end
       else
         respond_to do |format|
-          flash.now[:alert] = "Couldn't find user"
-          format.js { render partial: 'users/friend_result'}
+        flash.now[:alert] = "Couldn't find user"
+        format.js { render partial: 'users/friend_result'}
         end
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please enter a friend name or email"
-        format.js { render partial: 'users/friend_result'}
+      flash.now[:alert] = "Please enter a friend name or email"
+      format.js { render partial: 'users/friend_result'}
       end
     end
   end
